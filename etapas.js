7f9f05035d6e8cf2d4c43560fd16306d1b513a2d
Mateus Groh <mateus.groh@hotmail.com>
2022-08-14 21:38:19 -0300

    conexões DOM

---
+++ etapas.js
@@ -0,0 +1,55 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 13333,
+                nome: 'vereador 01',
+                partido: 'ABC',
+                fotos: [
+                    {url:'13333.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: 17777,
+                nome: 'vereador 02',
+                partido: 'DEF',
+                fotos: [
+                    {url:'17777.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 13,
+                nome: 'prefeito 01',
+                partido: 'ABC',
+                vice: 'vice 01',
+                fotos: [
+                    {url:'13.jpg', legenda: 'Prefeito'},
+                    {url:'13_02.jpg', legenda: 'Vice-Prefeito', small: true}
+                    
+                ]
+            },
+            {
+                numero: 17,
+                nome: 'prefeito 02',
+                partido: 'DEF',
+                vice: 'vice 02',
+                fotos: [
+                    {url:'17.jpg', legenda: 'Prefeito'},
+                    {url:'17_02.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    },
+
+
+
+
+]
---
+++ script.js
@@ -0,0 +1,22 @@
+let seuVotoPara = document.querySelector('.d-1-left-1 span');
+let cargo = document.querySelector('.d-1-left-2 span');
+let descricao = document.querySelector('.d-1-left-4');
+let aviso = document.querySelector('.d-2');
+let fotos = document.querySelector('.d-1-right');
+let numeros = document.querySelector('.d-1-left-3');
+
+function clicou (n) {
+    alert('clicou em ' +n);
+}
+
+function branco() {
+    alert('clicou em BRANCO!');
+}
+
+function corrige() {
+    alert('clicou em CORRIGE!');
+}
+
+function confirma() {
+    alert('clicou em CONFIRMA!');
+}
